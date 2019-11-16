15a0111212b0affc77f9d44c8de1a67048dbc853
Kevin <kevinhmai@outlook.com>
2019-11-15 22:05:03 -0800

    second commit

---
+++ possible_parentheses/cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mclovin/Desktop/homework5/problem_1/possible_parentheses/test
+# Build directory: /home/mclovin/Desktop/homework5/problem_1/possible_parentheses/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit "/home/mclovin/Desktop/homework5/problem_1/possible_parentheses/cmake-build-debug/unit_test")
+set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/home/mclovin/Desktop/homework5/problem_1/possible_parentheses/test/CMakeLists.txt;12;add_test;/home/mclovin/Desktop/homework5/problem_1/possible_parentheses/test/CMakeLists.txt;0;")
--- possible_parentheses/src/possible_parentheses.h
+++ possible_parentheses/src/possible_parentheses.h
@@ -1,9 +1,45 @@
 #include <list>
+#include "LinkedStack.h"
 
 // n: size of the problem
 // result: a list of string, each string represents a case of valid parentheses of size n
 void possibleParenthesis(int n, std::list<std::string> & result)
 {
     // homework
+    LinkedStack<std::string> stack;
+    int open, close =0;
+
+    /*while(!stack.isEmpty()){
+        stack.pop();
+        if(open==n && close==n){ // if we've consumed all left braces and right braces
+            result.push_back(stack.peek()); // it will be a valid result
+        }else{
+            if(open<n){ // if we can still use a left brace
+                stack.push(stack.peek()+'(');
+                open+=1;
+            }
+            if(close<open){ // if we can still use a right brace
+                stack.push(stack.peek()+')');
+                close+=1;
+            }
+        }
+    }*/
+     while(!stack.isEmpty()){
+
+        stack.pop();
+        if(open==n && close==n){ // if we've consumed all left braces and right braces
+            result.push_back(stack.peek()); // it will be a valid result
+        }else{
+            if(open<n){ // if we can still use a left brace
+                stack.push(stack.peek()+'(');
+                open+=1;
+            }
+            if(close<open){ // if we can still use a right brace
+                stack.push(stack.peek()+')');
+                close+=1;
+            }
+        }
+    }
+    //printf(result);
 }
 
