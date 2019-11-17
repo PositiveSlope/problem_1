89ea775fc0f3f449d8e6bdde8fed27faea40db46
Kevin <kevinhmai@outlook.com>
2019-11-16 18:06:00 -0800

    final commit

--- possible_parentheses/src/possible_parentheses.h
+++ possible_parentheses/src/possible_parentheses.h
@@ -1,45 +1,35 @@
 #include <list>
-#include "LinkedStack.h"
 
 // n: size of the problem
 // result: a list of string, each string represents a case of valid parentheses of size n
+void _possibleParenthesis(int n, int open, int close, std::list<std::string> & result)
+{
+    if(open==n && close == n) {
+        return;
+    }else {
+
+        if (open > close) {
+
+            _possibleParenthesis(n, open, close + 1, result);
+            result.emplace_back("{");
+            return;//w/o return, I would get size 7 for n=2, and size 21 for n=3
+        }
+
+        if (open < n) {//adding else if gave me the best result
+
+            _possibleParenthesis(n, open + 1, close, result);
+            result.emplace_back("}");//when debugging, it looked like my code left its recursion function, it went to the line before
+            return;
+        }//so i moved emplaceback after the call and switched { and }
+    }
+}//i drew out the state flows and when they drew things, but i couldnt figure out why it branched the way it did
+
 void possibleParenthesis(int n, std::list<std::string> & result)
 {
     // homework
-    LinkedStack<std::string> stack;
-    int open, close =0;
-
-    /*while(!stack.isEmpty()){
-        stack.pop();
-        if(open==n && close==n){ // if we've consumed all left braces and right braces
-            result.push_back(stack.peek()); // it will be a valid result
-        }else{
-            if(open<n){ // if we can still use a left brace
-                stack.push(stack.peek()+'(');
-                open+=1;
-            }
-            if(close<open){ // if we can still use a right brace
-                stack.push(stack.peek()+')');
-                close+=1;
-            }
-        }
-    }*/
-     while(!stack.isEmpty()){
-
-        stack.pop();
-        if(open==n && close==n){ // if we've consumed all left braces and right braces
-            result.push_back(stack.peek()); // it will be a valid result
-        }else{
-            if(open<n){ // if we can still use a left brace
-                stack.push(stack.peek()+'(');
-                open+=1;
-            }
-            if(close<open){ // if we can still use a right brace
-                stack.push(stack.peek()+')');
-                close+=1;
-            }
-        }
+    if(n>0){
+        _possibleParenthesis(n,0,0,result);
     }
-    //printf(result);
-}
 
+
+}
--- possible_parentheses/test/unit_tests.cpp
+++ possible_parentheses/test/unit_tests.cpp
@@ -1,24 +1,51 @@
 #include "gtest/gtest.h"
 #include "possible_parentheses.h"
-
 void showResult(std::list<std::string> & result) {
     for (std::list<std::string>::iterator i = result.begin(); i!=result.end(); ++i) {
-        printf("%s\n", (*i).c_str());
+        printf("%s", (*i).c_str());
     }
 };
 
+TEST(test_recursive, 0) {
+    std::list<std::string> result;
+    possibleParenthesis(0, result);
+    showResult(result);
+    ASSERT_TRUE(result.size()==0);
+}
+
 TEST(test_recursive, 1) {
     std::list<std::string> result;
+    int left, right=0;
     possibleParenthesis(1, result);
 
     // homework: add verification
+    showResult(result);//{}
+    /*for (std::list<std::string>::iterator i = result.begin(); i!=result.end(); ++i) {
+        if((*i).c_str()=="{"){
+            left++;
+        }
+        if((*i).c_str()=="}"){
+            right++;
+        }
+
+    }
+    std::cout<<left<<std::endl;
+    std::cout<<right<<std::endl;*/
+    ASSERT_TRUE(result.size()==2);//{}
 }
 
-TEST(test_recursive, 2) {
+TEST(test_recursive, 2) {//I traced debug to follow an FSM
+    //where (n, int open, int close) is as follows
+    //2,0,0->2,1,0->2,1,1->2,2,1->2,2,2->return/exit back to 2,2,1->return back to 2,1,1
+    //return back to 2,1,0 where (open < n), so open+1 ->2,2,0 -> (open>close) ->2,2,1
+    //again to 2,2,2
     std::list<std::string> result;
     possibleParenthesis(2, result);
 
     // homework: add verification
+    showResult(result);
+    std::cout<<result.size();
+    ASSERT_TRUE(result.size()==4);//{{}} and {}{}
 }
 
 TEST(test_recursive, 3) {
@@ -26,6 +53,10 @@ TEST(test_recursive, 3) {
     possibleParenthesis(3, result);
 
     // homework: add verification
+    showResult(result);
+    std::cout<<result.size();
+    ASSERT_TRUE(result.size()==6);//{{{}}} of size 6
+    // {}{}{}, {}{{}}, {{}}{}, {{}{}}, {{{}}}
 }
 
-// and more
+// and more, n=0
