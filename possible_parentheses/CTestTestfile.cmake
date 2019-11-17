076bfb8c9326f929d9600e84328833fc9a7e17dd
Kevin <kevinhmai@outlook.com>
2019-11-16 21:35:37 -0800

    final commit

--- possible_parentheses/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mclovin/Desktop/homework5/problem_1/possible_parentheses
-# Build directory: /home/mclovin/Desktop/homework5/problem_1/possible_parentheses
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest/googletest-build")
-subdirs("test")
